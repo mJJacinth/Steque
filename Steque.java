4f9a51388bb0ebb73346d5e0484b2d929ef2e240
mJJacinth <jessicablessy.777@msitprogran.net>
2022-01-04 22:38:16 +0530

    public steque

--- Steque.java
+++ Steque.java
@@ -30,13 +30,22 @@ import java.util.NoSuchElementException;
  *
  */
 public class Steque<Item> implements Iterable<Item> {
+    private Node first,last;
+    private int n;
+    private class Node{
+        Item item:
+        Node next;
+
+    }
 
 
     /**
      * constructs a steque object.
      */
     public Steque() {
-
+        n=0;
+        first=last=null;
+        
     }
     
     
